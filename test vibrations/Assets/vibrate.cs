f92a79e3f4c76c26b4afd79d13011c4858e0987c
Josip <josip.drinovac.mo@gmail.com>
2021-05-25 17:04:14 +0200

    vibration

---
+++ test vibrations/Assets/vibrate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vibrate : MonoBehaviour
+{
+    public void Vibrate()
+    {
+        Handheld.Vibrate();
+    }
+}
